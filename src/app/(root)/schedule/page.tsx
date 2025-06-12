69f08ade8f550c70e7dbc89a0ac987fff1c1ce28
saumya-14 <saumya140904@gmail.com>
2025-06-12 23:33:18 +0530

    schedulepage

--- src/app/(root)/schedule/page.tsx
+++ src/app/(root)/schedule/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+"use client";
 
-function page() {
-  return (
-    <div>
-      schedule
-    </div>
-  )
+import LoaderUI from "@/components/LoaderUI";
+import { useUserRole } from "@/hooks/useUserRole";
+import { useRouter } from "next/navigation";
+import InterviewScheduleUI from "./InterviewScheduleUI";
+
+function SchedulePage() {
+  const router = useRouter();
+
+  const { isInterviewer, isLoading } = useUserRole();
+
+  if (isLoading) return <LoaderUI />;
+  if (!isInterviewer) return router.push("/");
+
+  return <InterviewScheduleUI />;
 }
-
-export default page
+export default SchedulePage;
